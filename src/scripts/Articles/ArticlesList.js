e7456549abec05c1339e921c568285742170ad60
Goody <mattgoodman3099@gmail.com>
2020-09-24 15:51:19 -0500

    corrected fetch call for getArticles. articleList renders to DOM.

--- src/scripts/Articles/ArticlesList.js
+++ src/scripts/Articles/ArticlesList.js
@@ -1,1 +1,24 @@
-const eventHub = document.querySelector(".dashboard")
+import { getArticles, useArticles } from "./ArticlesDataProvider.js"
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".articles")
+
+const render = () => {
+    contentTarget.innerHTML = useArticles().map(article => {
+        return articleHTML(article)
+    })
+}
+
+const articleHTML = (articleObject) => {
+    return `
+        <div>Title: ${articleObject.title}</div>
+        <div>Summary: ${articleObject.summary}</div>
+        <div>Link: ${articleObject.URL}</div>
+        <div>Submitted by: ${articleObject.user.username}</div>
+    `
+}
+
+export const articleList = () => {
+    getArticles()
+    .then(render)
+}
