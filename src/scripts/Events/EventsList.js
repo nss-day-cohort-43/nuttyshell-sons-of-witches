8c8d4c17ce651a1d11b48b8120ae94b99189d401
fimoefive <brothermj@hotmail.com>
2020-09-24 14:58:00 -0500

    wrote the ColorList and renderEvent functions in the EventList.js file.

--- src/scripts/Events/EventsList.js
+++ src/scripts/Events/EventsList.js
@@ -1,5 +1,27 @@
-
+import { useEvents, getEvents } from "./EventsDataProvider.js";
 
 
 const contentEventTarget = document.querySelector(".events");
 const eventHub = document.querySelector(".dashboard");
+
+
+export const ColorList = () => {
+    getEvents()
+    .then(() => {
+        const eventArray = useEvents()
+        renderEvents(eventArray);
+    })
+};
+
+const renderEvents = (theEvents) => {
+    contentEventTarget.innerHTML = `
+    <select class="dropdown" id="eventSelect">
+        <option value="0">Select A Event ...</option>
+
+    ${theEvents.map(eventObj => {
+        return `<option value="${eventObj.id}">${eventObj.eventName}</option>`;
+        })
+     }
+     </select>
+    `
+};
